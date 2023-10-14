bd2ad6e6d6dbcf71a64239993d4c4879a72e1891
David Teixeira <118708351+DavidSilva32@users.noreply.github.com>
2023-10-14 03:29:01 -0300

    types and variables finished

--- types-and-variables/src/TypesVariables.java
+++ types-and-variables/src/TypesVariables.java
@@ -1,5 +1,15 @@
 public class TypesVariables {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        double minimumWage = 2500;
+
+        short shortNumber = 1;
+        int normalNumber = shortNumber;
+        short shortNumber2 = (short) normalNumber;
+
+        int number = 1;
+        number = 10;
+        System.out.print(number);
+
+        final double PI_VALUE = 3.14;
     }
 }
