375da86fe4479948b80f7706b0e0ae8e88d25db2
David Teixeira <118708351+DavidSilva32@users.noreply.github.com>
2023-10-14 22:40:11 -0300

    update of the flow-controller

--- conditional structures/flow-controller/src/SchoolResult.java
+++ conditional structures/flow-controller/src/SchoolResult.java
@@ -1,9 +1,12 @@
 public class SchoolResult {
     public static void main(String[] args) {
-        int media = 6;
+        double media = 6;
 
-        if (media >= 6) {
+        if (media >= 7) {
             System.out.println("Approved");
+        } else if (media >= 5 && media < 7) {
+            //It must return True or False 
+            System.out.println("Recovery Test");
         } else {
             System.out.println("Disapproved");
         }
